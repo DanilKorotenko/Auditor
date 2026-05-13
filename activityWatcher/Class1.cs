33c6aeb20ff5c46bfa4df3f1b94adc139a9b0974
DanilKorotenko <danil.korotenko@gmail.com>
2026-05-13 13:53:00 +0300

    User watcher partial implementation.

--- activityWatcher/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace activityWatcher;
-
-public class Class1
-{
-
-}
---
+++ userWatcher/ActivityWatcher/Activity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace userWatcher.ActivityWatcher;
+
+public class Activity
+{
+    public Activity()
+    {
+        ProcessName = string.Empty;
+        ProcessExePath = string.Empty;
+        UserName = string.Empty;
+        AdditionalInfo = new Dictionary<string, string>();
+    }
+    public string ProcessName { get; }
+    public string ProcessExePath { get; }
+    public string UserName { get; }
+    public Dictionary<string, string> AdditionalInfo { get; }
+
+}
