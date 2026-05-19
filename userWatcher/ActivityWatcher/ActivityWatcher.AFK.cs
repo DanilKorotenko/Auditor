2e1057abf00f974272282684d3a152cc0fa6b72e
DanilKorotenko <danil.korotenko@gmail.com>
2026-05-19 17:40:42 +0300

    AFK functionality moved to separate source file.

---
+++ userWatcher/ActivityWatcher/ActivityWatcher.AFK.cs
@@ -0,0 +1,44 @@
+﻿using common;
+using System.Runtime.InteropServices;
+
+namespace userWatcher.ActivityWatcher;
+
+[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+
+public partial class ActivityWatcher
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct LASTINPUTINFO
+    {
+        public uint cbSize; // The size of the structure in bytes
+        public uint dwTime; // The tick count when the last input was received
+    }
+
+    [DllImport("user32.dll")]
+    static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+    private bool IsAFK
+    {
+        get
+        {
+            uint idleTime = GetIdleTime();
+            if (idleTime > PreferencesManager.AfkTimeout)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public static uint GetIdleTime()
+    {
+        LASTINPUTINFO lastInput = new LASTINPUTINFO();
+        lastInput.cbSize = (uint)Marshal.SizeOf(lastInput); // This must be set manually
+
+        if (GetLastInputInfo(ref lastInput))
+        {
+            return (uint)Environment.TickCount - lastInput.dwTime;
+        }
+        return 0;
+    }
+}
--- userWatcher/ActivityWatcher/ActivityWatcher.cs
+++ userWatcher/ActivityWatcher/ActivityWatcher.cs
@@ -11,7 +11,7 @@ namespace userWatcher.ActivityWatcher;
 
 [System.Runtime.Versioning.SupportedOSPlatform("windows")]
 
-public class ActivityWatcher
+public partial class ActivityWatcher
 {
     [DllImport("user32.dll")]
     private static extern IntPtr GetForegroundWindow();
@@ -24,16 +24,6 @@ public class ActivityWatcher
 
     [DllImport("user32.dll")]
     private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
-
-    [StructLayout(LayoutKind.Sequential)]
-    struct LASTINPUTINFO
-    {
-        public uint cbSize; // The size of the structure in bytes
-        public uint dwTime; // The tick count when the last input was received
-    }
-
-    [DllImport("user32.dll")]
-    static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
 
 
     private readonly int DEFAULT_UPDATE_INTERVAL = 1;
@@ -87,33 +77,6 @@ public class ActivityWatcher
 
         return result;
     }
-
-    private bool IsAFK
-    {
-        get
-        {
-            uint idleTime = GetIdleTime();
-            logger.LogInformation($"Idle time {idleTime}");
-            if (idleTime > PreferencesManager.AfkTimeout)
-            {
-                return true;
-            }
-            return false;
-        }
-    }
-
-    public static uint GetIdleTime()
-    {
-        LASTINPUTINFO lastInput = new LASTINPUTINFO();
-        lastInput.cbSize = (uint)Marshal.SizeOf(lastInput); // This must be set manually
-
-        if (GetLastInputInfo(ref lastInput))
-        {
-            return (uint)Environment.TickCount - lastInput.dwTime;
-        }
-        return 0;
-    }
-
 
     private void Shutdown()
     {
